423e554d75ec989f7e7c9e6b99cbbcfe8d466f84
W0834082 <W0834082@ADL-A1201I-29>
2023-10-13 13:31:45 -0400

    MAking changes in css and add new values in evry page for images and make it good

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace JarCreations.Controllers
         {
             return View();
         }
-
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
